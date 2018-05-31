a7572791f2b41a47a08a09c718e24720af064bef
Oleksandr Berezhnyi <oleksandr.berezhnyi@elements.nl>
2018-05-31 10:09:41 +0200

    Chapter 10

--- RxSwiftExamples/RxHelper/RxHelper+Chapter9.swift
+++ RxSwiftExamples/RxHelper/RxHelper+Chapter9.swift
@@ -8,6 +8,14 @@ extension RxHelper {
         example(title: "concut", action: concat)
         example(title: "concatOneElement", action: concatOneElement)
         example(title: "merge", action: merge)
+        example(title: "combineLatest", action: combineLatest)
+        example(title: "zip", action: zip)
+        example(title: "withLatestFrom", action: withLatestFrom)
+        example(title: "sample", action: sample)
+        example(title: "amb", action: amb)
+        example(title: "switchLatest", action: switchLatest)
+        example(title: "reduce", action: reduce)
+        example(title: "scan", action: scan)
     }
     
     private static func startWith() {
@@ -34,6 +42,7 @@ extension RxHelper {
             })
             .disposed(by: disposeBag)
         
+        //
         let observable2 = first.concat(second)
         observable2
             .subscribe(onNext: { (value) in
@@ -86,5 +95,195 @@ extension RxHelper {
         first.onCompleted()
         second.onCompleted()
         second.onError(RxError.defaultError)
+    }
+    
+    private static func combineLatest() {
+        let disposeBag = DisposeBag()
+        
+        let first = PublishSubject<String>()
+        let second = PublishSubject<String>()
+        let observable = Observable.combineLatest(first, second) { v1, v2 in
+            return (v1, v2)
+        }
+        observable
+            .subscribe { (event) in
+                print(event)
+            }
+            .disposed(by: disposeBag)
+        
+        first.onNext("1) A")
+        first.onNext("1) B")
+        first.onNext("1) C")
+        second.onNext("2) D")
+        second.onNext("2) E")
+        second.onNext("2) F")
+        first.onNext("1) G")
+        second.onNext("2) H")
+        first.onCompleted()
+        second.onCompleted()
+        second.onError(RxError.defaultError)
+        
+        //
+        let bools = Observable<Bool>.of(true, false)
+        let numbers = Observable<Int>.of(1, 2, 3)
+        let observable2 = Observable<Int>.combineLatest(bools, numbers) { v1, v2 in
+            if v1 {
+                return v2
+            }
+            return -v2
+        }
+        observable2
+            .subscribe { (event) in
+                print("2) \(event)")
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private static func zip() {
+        let disposeBag = DisposeBag()
+        
+        let first = PublishSubject<String>()
+        let second = PublishSubject<String>()
+        let observable = Observable.zip(first, second) { v1, v2 in
+            return (v1, v2)
+        }
+        observable
+            .subscribe { (event) in
+                print(event)
+            }
+            .disposed(by: disposeBag)
+        
+        first.onNext("1) A")
+        first.onNext("1) B")
+        first.onNext("1) C")
+        second.onNext("2) D")
+        second.onNext("2) E")
+        second.onNext("2) F")
+        first.onNext("1) G")
+        second.onNext("2) H")
+        first.onCompleted()
+        second.onCompleted()
+        second.onError(RxError.defaultError)
+    }
+    
+    private static func withLatestFrom() {
+        let disposeBag = DisposeBag()
+        
+        let button = PublishSubject<Void>()
+        let textField = PublishSubject<String>()
+        
+        let observable = button.withLatestFrom(textField)
+        observable
+            .subscribe { (event) in
+                print(event)
+            }
+            .disposed(by: disposeBag)
+        
+        textField.onNext("Par")
+        textField.onNext("Pari")
+        textField.onNext("Paris")
+        button.onNext(())
+        button.onNext(())
+    }
+    
+    private static func sample() {
+        let disposeBag = DisposeBag()
+        
+        let button = PublishSubject<Void>()
+        let textField = PublishSubject<String>()
+        
+        let observable = textField.sample(button)
+        observable
+            .subscribe { (event) in
+                print(event)
+            }
+            .disposed(by: disposeBag)
+        
+        textField.onNext("Par")
+        textField.onNext("Pari")
+        textField.onNext("Paris")
+        button.onNext(())
+        button.onNext(())
+    }
+    
+    private static func amb() {
+        let disposeBag = DisposeBag()
+        
+        let first = PublishSubject<String>()
+        let second = PublishSubject<String>()
+        
+        let observable = first.amb(second)
+        observable
+            .subscribe(onNext: { value in
+                print(value)
+            })
+            .disposed(by: disposeBag)
+        
+        first.onNext("Lisbon")
+        second.onNext("Copenhagen")
+        first.onNext("London")
+        first.onNext("Madrid")
+        second.onNext("Vienna")
+    }
+    
+    private static func switchLatest() {
+        // It's similar to `flatMapLatest` (chapter 7)
+        let disposeBag = DisposeBag()
+        
+        let one = PublishSubject<String>()
+        let two = PublishSubject<String>()
+        let three = PublishSubject<String>()
+        let source = PublishSubject<Observable<String>>()
+        
+        let observable = source.switchLatest()
+        observable
+            .subscribe(onNext: { value in
+                print(value)
+            })
+            .disposed(by: disposeBag)
+        
+        source.onNext(one)
+        one.onNext("Some text from sequence one")
+        two.onNext("Some text from sequence two")
+        
+        source.onNext(two)
+        two.onNext("More text from sequence two")
+        one.onNext("and also from sequence one")
+        
+        source.onNext(three)
+        two.onNext("Why don't you seem me?")
+        one.onNext("I'm alone, help me")
+        three.onNext("Hey it's three. I win.")
+        
+        source.onNext(one)
+        one.onNext("Nope. It's me, one!")
+    }
+    
+    private static func reduce() {
+        let disposeBag = DisposeBag()
+        
+        let source = Observable.of(1, 3, 5, 7, 9)
+        
+        let observable = source.reduce(0, accumulator: { summary, newValue in
+            return summary + newValue
+        })
+        observable
+            .subscribe(onNext: { value in
+                print(value)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private static func scan() {
+        let disposeBag = DisposeBag()
+        
+        let source = Observable.of(1, 3, 5, 7, 9)
+        let observable = source.scan(0, accumulator: +)
+        observable
+            .subscribe(onNext: { value in
+                print(value)
+            })
+            .disposed(by: disposeBag)
     }
 }
